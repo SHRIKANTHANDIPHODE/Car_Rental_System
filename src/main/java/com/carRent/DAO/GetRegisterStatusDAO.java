ab21481edc13ff64ccbef5e6ce7de8ed6551062f
Kundan Handiphode <shreehandiphode333@gmail.com>
2024-06-25 19:59:58 +0530

    Initial Commit

---
+++ src/main/java/com/carRent/DAO/GetRegisterStatusDAO.java
@@ -0,0 +1,28 @@
+package com.carRent.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.carRent.CommonPacakage.DBConnection;
+
+public class GetRegisterStatusDAO 
+{
+
+	public static boolean getStatus(String email,long aadhar) throws SQLException
+	{
+		Connection con=DBConnection.getDBConnection();
+		try {
+			PreparedStatement pstmt=con.prepareStatement("select * from registercarusers where email=? or aadharno=?");
+			pstmt.setString(1, email);
+			pstmt.setLong(2, aadhar);
+			int k = pstmt.executeUpdate();
+			if(k==1)
+				return false;
+			else
+				return true;
+		} catch (SQLException e) {
+			return true;
+		}
+	}
+}
